4922ec9b5dbe93136a9bbcaf5c31e888e8f129a3
Hill Pan <hill168@outlook.com>
2025-02-16 23:08:06 +0800

    updated example a little

--- example/src/plugins/vuetify.ts
+++ example/src/plugins/vuetify.ts
@@ -1,7 +1,8 @@
-// src/plugins/vuetify.ts
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
+import "@mdi/font/css/materialdesignicons.css";
+import 'vuetify/dist/vuetify.min.css';
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
 
 export default createVuetify({
     components,
--- h-copy-label/src/plugins/vuetify.ts
+++ h-copy-label/src/plugins/vuetify.ts
@@ -1,4 +1,3 @@
-// Styles
 import "@mdi/font/css/materialdesignicons.css";
 import 'vuetify/dist/vuetify.min.css';
 import { createVuetify } from 'vuetify';
